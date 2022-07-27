27139205f095acc54c6fb8f8cb4495fb25a9505c
Samandar Rajabboyev <rajabboyevsamandar22@gmail.com>
2022-07-27 12:56:43 +0500

    init

---
+++ android/app/src/main/kotlin/uz/pdp/herewego/MainActivity.kt
@@ -0,0 +1,6 @@
+package uz.pdp.herewego
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:herewego/pages/home_page.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
